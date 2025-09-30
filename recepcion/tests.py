1ef894a84cd6bc989da0990db835728275d43314
Diegohenriquez17 <diegohen2005gonzales@gmail.com>
2025-09-30 20:59:48 -0300

    CI: add GitHub Actions workflow and smoke test

--- recepcion/tests.py
+++ recepcion/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class SmokeTests(TestCase):
+	def test_home_redirects_or_loads(self):
+		c = Client()
+		resp = c.get('/')
+		# either redirect to login or render home; just assert it's a valid response
+		self.assertIn(resp.status_code, (200, 302))
