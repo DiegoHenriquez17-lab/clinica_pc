fd4915b4b9f063167d0038d527d5347b0c31d068
JAHAZIEL HEMAN IGOR VIDAL <jahaziel.igor@inacapmail.cl>
2025-10-06 16:26:39 -0300

    ''

--- entrega/admin.py
+++ entrega/admin.py
@@ -6,19 +6,19 @@ from .models import Entrega
 @admin.register(Entrega)
 class EntregaAdmin(admin.ModelAdmin):
 	list_display = ("diagnostico_cliente", "diagnostico_equipo", "recibido_por", "observaciones_short", "fecha_entrega")
-	search_fields = ("diagnostico__cliente__nombre", "recibido_por", "diagnostico__equipo__nombre")
-	list_select_related = ("diagnostico", "diagnostico__cliente", "diagnostico__equipo")
+	search_fields = ("equipo__diagnostico__cliente__nombre", "recibido_por", "equipo__tipo_equipo")
+	list_select_related = ("equipo", "equipo__diagnostico", "equipo__cliente", "responsable")
 
 	def diagnostico_cliente(self, obj):
-		return obj.diagnostico.cliente.nombre if obj.diagnostico and obj.diagnostico.cliente else ""
+		return obj.equipo.diagnostico.cliente.nombre if obj.equipo.diagnostico and obj.equipo.diagnostico.cliente else ""
 	diagnostico_cliente.short_description = "Cliente"
 
 	def diagnostico_equipo(self, obj):
-		return str(obj.diagnostico.equipo) if obj.diagnostico and obj.diagnostico.equipo else ""
+		return str(obj.equipo) if obj.equipo else ""
 	diagnostico_equipo.short_description = "Equipo"
 
 	def observaciones_short(self, obj):
-		text = (obj.observaciones or "")
+		text = (obj.observaciones_entrega or "")
 		return (text[:75] + "…") if len(text) > 75 else text
 	observaciones_short.short_description = "Observaciones"
 
